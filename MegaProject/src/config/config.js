7ebc4408aeb9931798ddfd545a12ec0d34b8b084
Rahul <rahuldey.333@gmail.com>
2024-06-04 19:41:56 +0530

    Added Appwrite environment variables

---
+++ MegaProject/src/config/config.js
@@ -0,0 +1,16 @@
+const conf = {
+    appwriteUrl : String(import.meta.env.
+        VITE_APP_APPWRITE_URL),
+    appwriteDatabseId: String(import.meta.env.
+        VITE_APP_APPWITE_DATABASE_ID),
+    appwriteProjectId : String(import.meta.env.
+            VITE_APP_APPWRITE_PROJECT_ID),
+    appwriteCollectionsId : String(import.meta.env.
+                VITE_APP_APPWRITE_COLLECTIONS_ID),
+    appwriteBucketId : String(import.meta.env.
+                    VITE_APP_APPWRITE_COLLECTIONS_ID),
+    
+}
+
+
+export default conf
